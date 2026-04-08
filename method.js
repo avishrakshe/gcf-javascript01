6b792adf1d492f00e69a41978f7686817a6d34b1
Avish <avishraksheiitb@gmail.com>
2026-04-08 15:00:59 +0530

    Add files via upload

---
+++ method.js
@@ -0,0 +1,18 @@
+let a=[1,2];
+let b=[3,4];
+
+
+
+//concat()
+let result=a.concat(b);
+console.log(result);
+
+
+//join()
+let str=result.join("");
+console.log(str);
+
+let numbers=[10,20,30,40,50];
+//slice()
+let sliced=numbers.slice(1,4);
+console.log(sliced);

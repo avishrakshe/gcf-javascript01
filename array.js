0fb24f755808a6393fbc64ebd7c4d42d2a8b8f29
Avish <avishraksheiitb@gmail.com>
2026-04-08 13:24:39 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,11 @@
+//creating an array 
+let numbers = [10,20,30,40,50];
+
+//printing the aaray
+console.log(numbers);
+
+//accessing first element of the array
+console.log(numbers[0]);
+
+//accessing last element of the array
+console.log(numbers[numbers.length - 1]);
